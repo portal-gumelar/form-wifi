0d1c1ab32bd57e791a44666f194ca1e754cd52be
Sukarno Anomali <sukarnoanomali69@gmail.com>
2026-05-12 17:02:54 +0700

    build: trigger fresh vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Last update: 2026-05-12 17:03
 import React, { useState } from "react";
 import { RegistrationForm } from "./pages/RegistrationForm";
 import { SuccessPage } from "./components/ui/SuccessPage";
